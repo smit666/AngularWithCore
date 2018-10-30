e7c05b223c75fbdc7d5241626861848fbf9a6644
smit666 <smitthakur666@gmail.com>
2018-10-30 18:34:06 +0530

    changes

--- AngularWithCore/src/app/app.component.ts
+++ AngularWithCore/src/app/app.component.ts
@@ -1,10 +1,20 @@
-import { Component } from '@angular/core';
+import { Component,OnInit } from '@angular/core';
+import { Http } from '@angular/http';
 
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
   styleUrls: ['./app.component.css']
 })
-export class AppComponent {
-  title = 'AngularProject';
+export class AppComponent implements OnInit {
+  apiValues: string[] = [];
+  constructor(private _service: Http) {
+
+  }
+  ngOnInit() {
+    this._service.get("/api/values").subscribe(result => {
+      this.apiValues = result.json() as string[];  
+    });
+  }
+ // title = 'AngularProject';
 }
